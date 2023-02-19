56c3029bba9ab05b94fbe02d8b7365d9d2b9af62
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-02-19 10:33:04 -0500

    Update

--- functions/fakeuser.js
+++ functions/fakeuser.js
@@ -1,31 +1,56 @@
-function generateFakeUserData(number) {
-
-const names = ['John', 'Paul', 'George', 'Ringo', 'Mary', 'Sue', 'Bob', 'Carol']
-const emails = ['john@example.com', 'paul@example.com', 'george@example.com', 'ringo@example.com', 'mary@example.com', 'sue@example.com', 'bob@example.com', 'carol@example.com']
-const passwords = [
-  "password1",
-  "password2",
-  "password3",
-  "password4",
-  "password5",
-  "password6",
-  "password7",
-  "password8",
-];  
-const fakeUserData = [];
-
-  for(let i = 0; i < number; i++) {
-    fakeUserData.push({
-      name: names[i],
-      email: emails[i],
-      password: passwords[i]
-    });
+const { hash, genSalt } = require("bcrypt");
+const fs = require("fs")
+const path = require('path');
+ const names = [];
+ const emails = [];
+ const passwords = [];
+const namesfile = fs
+  .readFileSync(path.join(__dirname, "./data/names.txt"), "utf-8")
+  .split("\n");
+  const emailsfile = fs
+    .readFileSync(path.join(__dirname, "./data/emails.txt"), "utf-8")
+    .split("\n");
+    const passwordsfile = fs
+      .readFileSync(path.join(__dirname, "./data/passwords.txt"), "utf-8")
+      .split("\n");
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function MD5Hash(string) {
+  //Step 1: Convert the input string to bytes
+  let bytes = [];
+  for (let i = 0; i < string.length; i++) {
+    bytes.push(string.charCodeAt(i));
   }
-
-
+  //Step 2: Calculate the MD5 hash
+  let hash = CryptoJS.MD5(CryptoJS.enc.Latin1.parse(bytes));
+  //Step 3: Return the hash as a hex string
+  return hash.toString(CryptoJS.enc.Hex);
+}
+ async function generateFakeUserData(number) {
+ for (const name of namesfile) {
+   name.toLocaleLowerCase();
+   names.push(name);
+ }
+ for (const email of emailsfile) {
+   email.toLocaleLowerCase();
+   emails.push(email);
+ }
+ for (const password of passwordsfile) {
+   password.toLocaleLowerCase();
+   passwords.push(password);
+ } 
+const fakeUserData = [];
+  for(let i = 0; i < number; i++) {
+   fakeUserData.push({
+     id: rand(1, 100),
+     name: names[i],
+     email: emails[i],
+     password: passwords[i],
+   });
+  }
   return fakeUserData;
 }
-
 module.exports = {
   generateFakeUserData,
 };
--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const { generateFakeUserData } = require('./functions/fakeuser')
 app.use(express.json())
 app.use(urlencoded({extended: false}))
 const  users = []
-
 app.use(express.static("./public"))
 app.get('/video', (req, res) =>
 {
@@ -19,27 +18,21 @@ app.get('/user',(req, res) =>{
 app.get('/', (req ,res) =>{
   res.render('index.ejs',)
 })
-
 app.get("/fake/users",async (req,res)=>{
 try {
-    const number = req.query.number;
+    const number = req.query.number 
     const variations = req.query.variations;
     if (!number) {
-      res.send("Pls Provide The Number of Users To Create").status(400);
-      return;
+      res.send("Pls Provide A NUmber").status(400)
+      return
     }
-    
 const data = await generateFakeUserData(number)
+console.log( data.length)
     res.json(data)
 } catch (e) {
   res.send(e).status(500)
-  
 }
-
 })
-
-
-
 app.listen(1200,()=>{
   console.log("Server Started");
 })
